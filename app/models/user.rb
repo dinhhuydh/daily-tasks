4d6ae326b19926315a6f9fb08a0cf7d34fbdbead
huydinh <dinhhuydh@gmail.com>
2020-09-24 12:53:28 +0700

    Password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,16 @@
 class User < ApplicationRecord
   has_secure_password
   validates_length_of :username, minimum: 4, maximum: 16
+  validates_format_of :username, :with => /\A[a-zA-Z0-9_\-]+\z/
+
+  validates_length_of :password, minimum: 8, maximum: 16
+  validate :strong_password_checker
+
+  private
+
+  def strong_password_checker
+    if !password.match(/[A-Z]/) || !password.match(/[a-z]/) || !password.match(/[0-9]/)
+      errors.add(:password, 'Not contains at least 1 upper case, 1 lower case and 1 number')
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:password) { 'Abc123456' }
-  describe '#validate' do
+  describe '#validate username' do
+    let(:password) { 'Abc123456' }
+
     it 'has username length greater than or equal 4' do
       user = User.new(username: 'abc', password: password)
       expect(user.valid?).to be_falsy
@@ -10,13 +11,47 @@ RSpec.describe User, type: :model do
 
     it 'has username length less than or equal 16' do
       invalid_username = 'a' * 17
-      user = User.new(username: invalid_username)
+      user = User.new(username: invalid_username, password: password)
       expect(user.valid?).to be_falsy
     end
 
     it 'has username length between 4 and 16' do
       valid_username = 'a' * 15
       user = User.new(username: valid_username, password: password)
+      expect(user.valid?).to be_truthy
+    end
+
+    it 'has username which does not contains special character' do
+      invalid_username = '!aaa'
+      user = User.new(username: invalid_username, password: password)
+      expect(user.valid?).to be_falsy
+    end
+  end
+
+  describe '#validate password' do
+    let(:username) { 'username' }
+
+    it 'has password length greater than or equal 8' do
+      password = 'abc123'
+      user = User.new(username: username, password: password)
+      expect(user.valid?).to be_falsy
+    end
+
+    it 'has password length less than or equal 16' do
+      password = 'a' * 17
+      user = User.new(username: username, password: password)
+      expect(user.valid?).to be_falsy
+    end
+
+    it 'has password length between 4 and 16' do
+      password = 'a' * 15
+      user = User.new(username: username, password: password)
+      expect(user.valid?).to be_falsy
+    end
+
+    it 'has password length between 4 and 16 and have uppercase, lowercase and number' do
+      password = 'abc123Abc'
+      user = User.new(username: username, password: password)
       expect(user.valid?).to be_truthy
     end
   end
